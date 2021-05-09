d58a5f6cd6cbc90ddcc186fc81285d5a773f8261
Jordan Williams <jordan@jwillikers.com>
2021-05-09 13:10:45 -0500

    Initial commit

---
+++ pinebook-pro-fedora-installer.fish
@@ -0,0 +1,9 @@
+set name (buildah from fedora)
+
+buildah run $name dnf -y install bash bsdtar btrfs-progs dialog git gawk libarchive openssl parted polkit qemu-user-static rsync systemd-container wget
+
+buildah run $name git clone https://github.com/bengtfredh/pinebook-pro-fedora-installer.git /root/pinebook-pro-fedora-installer
+
+buildah config --entrypoint "/usr/bin/bash /root/pinebook-pro-fedora-installer/fedora-installer" $name
+
+buildah commit $name pinebook-pro-fedora-installer
