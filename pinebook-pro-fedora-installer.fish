c6eee4d91167563da1e6397665b533128d0f25e6
Jordan Williams <jordan@jwillikers.com>
2021-05-10 07:41:22 -0500

    Remove dependency on BSD tar

--- pinebook-pro-fedora-installer.fish
+++ pinebook-pro-fedora-installer.fish
@@ -1,6 +1,6 @@
 set name (buildah from fedora)
 
-buildah run $name dnf -y install bash bsdtar btrfs-progs dialog git gawk libarchive openssl parted polkit qemu-user-static rsync systemd-container wget
+buildah run $name dnf -y install bash btrfs-progs dialog git gawk libarchive openssl parted polkit qemu-user-static rsync systemd-container wget
 
 buildah run $name git clone https://github.com/bengtfredh/pinebook-pro-fedora-installer.git /root/pinebook-pro-fedora-installer
 
